011ab78e4f65ebe293c4a1c5cfe1784313b74e24
jun <flyt9097@gmail.com>
2021-03-08 18:47:48 +0900

    Update

--- shopping_mall_app/lib/screens/home.dart
+++ shopping_mall_app/lib/screens/home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'inkwellTest.dart';
+import 'package:shopping_mall_app/main.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -6,24 +8,41 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+/*
+  void showPage() {
+    setState((int index) {
+      _currentIndex = 4;
+    });
+  }
+
+  이거는 provider를 사용해야함 - 공부중
+*/
+
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Row(
-      children: [
-        InkWell(
-            child: Image.asset("images/images.jpeg", width: 30, height: 30),
-            onTap: () => showMessage("FAIL")),
-        InkWell(
-            child: Image.asset("screens/success.jpeg", width: 30, height: 30),
-            onTap: () => showMessage("SUCCESS"))
-      ],
-    ));
-  }
-}
-
-void showMessage(String msg) {
-  AppBar buildAppBar({String txt}) {
-    return AppBar(title: Center(child: Text("~ SUCCESS ~")));
+        body: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+      Row(children: [
+        Expanded(
+          child: InkWell(
+              child: Image.asset("lib/images/test1.jpeg", fit: BoxFit.fill)),
+          //onTap: () => showPage()
+        ),
+        Expanded(
+            child: InkWell(
+          child: Image.asset("lib/images/doge.jpg", fit: BoxFit.fill),
+          //onTap: () => showPage()
+        ))
+      ]),
+      Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+        Container(child: Text(" 1 INFORMATION 1 ")),
+        Container(child: Text(" 2 INFORMATION 2 "))
+      ]),
+      Container(child: Text(" GRAPHS ")),
+      Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+        Container(child: Text(" GRAPH 1 NUM ")),
+        Container(child: Text(" GRAPH 2 NUM "))
+      ])
+    ]));
   }
 }
 
